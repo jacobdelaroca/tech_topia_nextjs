2d04287ca60942b8b6a1ac520c29f7927c967d74
Jacob Dela Roca <156392871+jacobdelaroca@users.noreply.github.com>
2024-11-18 17:17:42 +0800

    disabled cache for qr generation

--- app/admin/generateQR/page.tsx
+++ app/admin/generateQR/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { getAreas } from '../serverActions';
 import QRPanel from './_components/QRPanel';
+import { unstable_noStore } from 'next/cache';
 const GenerateQR = async () => {
-    
+    unstable_noStore()
     const areas = await getAreas()
     return (
         <div className='grid grid-cols-2'>
